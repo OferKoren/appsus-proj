f96774fffb2b30cf7620f8d1fbada904910fe133
eranyosef2 <EranYosefHD@gmail.com>
2024-10-14 17:05:13 +0300

    Added delete mail functionality

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,19 +1,20 @@
 const { useNavigate } = ReactRouterDOM
 
-export function MailPreview({ mail }) {
+export function MailPreview({ mail, onRemoveMail }) {
     const { from, subject, isRead, date } = mail
     const navigate = useNavigate()
 
+    
     return (
-        <div
-            className={`mail-preview ${isRead ? 'read' : 'unread'}`}
-            onClick={() => navigate(`/mail/${mail.id}`)}
-        >
-            <span className="mail-from">{from}</span>
-            <span className="mail-subject">
+        <div className={`mail-preview ${isRead ? 'read' : 'unread'}`}>
+            <span className="mail-from" onClick={() => navigate(`/mail/${mail.id}`)}>{from}</span>
+            <span className="mail-subject" onClick={() => navigate(`/mail/${mail.id}`)}>
                 {subject.length > 25 ? `${subject.slice(0, 25)}...` : subject}
             </span>
             <span className="mail-date">{date}</span>
+            <button className="delete-btn" onClick={() => onRemoveMail(mail.id)}>
+                🗑️
+            </button>
         </div>
     )
 }
