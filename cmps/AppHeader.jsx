f454c5442400a5e41cb1002dbe600ba049652577
OferKoren <oferkoren2@gmail.com>
2024-10-18 05:53:54 +0300

    start dynamic appHeader

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -3,34 +3,14 @@ import { HeaderFilter } from './HeaderFilter.jsx'
 const { Link, NavLink } = ReactRouterDOM
 const { useState } = React
 
-export function AppHeader({ app, setApp, filterBy, onSetFilterBy }) {
-    return (
-        <header className="app-header full">
-            <Link to={`/${app}`}>
-                <div className="logo-wrapper">
-                    <span>{app}</span>
-                    <img src={`./assets/img/logos/${app}.png`} alt="" />
-                </div>
-            </Link>
-            <HeaderFilter app={app} filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
-            <nav className="header-nav">
-                <NavLink to="/" onClick={() => setApp('Appsus')}>
-                    Home
-                </NavLink>
-                <NavLink to="/about" onClick={() => setApp('Appsus')}>
-                    About
-                </NavLink>
-                <NavLink to="/mail" onClick={() => setApp('Mail')}>
-                    <div className="img-wrapper">
-                        <img src="./assets/img/gmail-icons/gmail-nav-icon2.png" alt="" />
-                    </div>
-                </NavLink>
-                <NavLink to="/note" onClick={() => setApp('Keep')}>
-                    <div className="img-wrapper">
-                        <img src="./assets/img/notes-icons/keep-nav-icon2.png" alt="" />
-                    </div>
-                </NavLink>
-            </nav>
-        </header>
-    )
+export function AppHeader(props) {
+    console.log(props)
+    switch (props.app) {
+        case 'Appsus':
+            return <AppHeaderBase {...props} />
+        case 'Note':
+            return <NoteHeader {...props} />
+        case 'Mail':
+            return <AppHeaderBase {...props} />
+    }
 }
---
+++ cmps/AppHeaderBase.jsx
@@ -0,0 +1,37 @@
+import { HeaderFilter } from './HeaderFilter.jsx'
+
+const { Link, NavLink } = ReactRouterDOM
+const { useState } = React
+
+export function AppHeaderBase({ app, setApp, filterBy, onSetFilterBy, childrens }) {
+    return (
+        <header className="app-header full">
+            <Link to={`/${app}`}>
+                <div className="logo-wrapper">
+                    <span>{app}</span>
+                    <img src={`./assets/img/logos/${app}.png`} alt="" />
+                </div>
+            </Link>
+            {childrens && childrens}
+            <HeaderFilter app={app} filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
+            <nav className="header-nav">
+                <NavLink to="/" onClick={() => setApp('Appsus')}>
+                    Home
+                </NavLink>
+                <NavLink to="/about" onClick={() => setApp('Appsus')}>
+                    About
+                </NavLink>
+                <NavLink to="/mail" onClick={() => setApp('Mail')}>
+                    <div className="img-wrapper">
+                        <img src="./assets/img/gmail-icons/gmail-nav-icon2.png" alt="" />
+                    </div>
+                </NavLink>
+                <NavLink to="/note" onClick={() => setApp('Keep')}>
+                    <div className="img-wrapper">
+                        <img src="./assets/img/notes-icons/keep-nav-icon2.png" alt="" />
+                    </div>
+                </NavLink>
+            </nav>
+        </header>
+    )
+}
